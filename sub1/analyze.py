cef50bba3e8a657752ff8fd514ea7093085b2708
YooSijin <yousj9461@gmail.com>
2021-09-02 22:05:09 +0900

    PJT2Sub1 Sijin
    Former-commit-id: 6cfbd00811b5e015e54cd856077d19ee9acbd8b3

--- sub1/analyze.py
+++ sub1/analyze.py
@@ -12,22 +12,39 @@ def sort_stores_by_score(dataframes, n=20, min_reviews=30):
         dataframes["stores"], dataframes["reviews"], left_on="id", right_on="store"
     )
     scores_group = stores_reviews.groupby(["store", "store_name"])
+    scores_group = scores_group.filter(lambda r: len(r) >= min_reviews).groupby(["store", "store_name"])
     scores = scores_group.mean()
-    return scores.head(n=n).reset_index().sort_values(by="score", ascending=False)
+    scores = scores.sort_values(by="score", ascending=False)
+
+    return scores.head(n=n).reset_index()
 
 
 def get_most_reviewed_stores(dataframes, n=20):
     """
     Req. 1-2-3 가장 많은 리뷰를 받은 `n`개의 음식점을 정렬하여 리턴합니다
     """
-    raise NotImplementedError
+    stores_reviews = pd.merge(
+        dataframes["stores"], dataframes["reviews"], left_on="id", right_on="store"
+    )
+    scores_group = stores_reviews.groupby(["store", "store_name"])
+    scores = scores_group.count()
+    scores = scores.sort_values(by="content", ascending=False)
+
+    return scores.head(n=n).reset_index()
 
 
 def get_most_active_users(dataframes, n=20):
     """
     Req. 1-2-4 가장 많은 리뷰를 작성한 `n`명의 유저를 정렬하여 리턴합니다.
     """
-    raise NotImplementedError
+    users_reviews = pd.merge(
+        dataframes["users"], dataframes["reviews"], on="id"
+    )
+    count = [0] * len(users_reviews)
+    users_reviews["count"] = count
+    users_group = users_reviews.groupby("id")
+    users_group = users_group.count().sort_values(by="content", ascending=False)
+    return users_group.head(n=n).reset_index()
 
 
 def main():
@@ -37,19 +54,35 @@ def main():
     separater = "-" * term_w
 
     stores_most_scored = sort_stores_by_score(data)
+    stores_most_reviewed = get_most_reviewed_stores(data)
+    users_most_reviewed = get_most_active_users(data)
 
     print("[최고 평점 음식점]")
     print(f"{separater}\n")
-    # for i, store in stores_most_scored.iterrows():
-    #     print(
-    #         "{rank}위: {store}({score}점)".format(
-    #             rank=i + 1, store=store.store_name, score=store.score
-    #         )
-    #     )
     for i, store in stores_most_scored.iterrows():
         print(
-            "{rank}위: {store}({score}점)".format(
+            "{rank}위: {store}({score:.4f}점)".format(
                 rank=i + 1, store=store.store_name, score=store.score
+            )
+        )
+    print(f"\n{separater}\n\n")
+
+    print("[최다 리뷰 음식점]")
+    print(f"{separater}\n")
+    for i, store in stores_most_reviewed.iterrows():
+        print(
+            "{rank}위: {store} (리뷰 : {review}개)".format(
+                rank=i + 1, store=store.store_name, review=store.content
+            )
+        )
+    print(f"\n{separater}\n\n")
+
+    print("[최다 리뷰 유저]")
+    print(f"{separater}\n")
+    for i, user in users_most_reviewed.iterrows():
+        print(
+            "{rank}위: {id} (리뷰 : {review}개)".format(
+                rank=i + 1, id=user.id, review=user.content
             )
         )
     print(f"\n{separater}\n\n")
