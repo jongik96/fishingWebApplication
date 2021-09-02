1dbde45a70d2e76bbcbca25bb1130e4a5f5b5bb9
박종익 <pji3504@naver.com>
2021-09-02 09:59:47 +0900

    test
    Former-commit-id: 2894be7a51003395f7f177f9546ccfb8943ca18b

--- sub1/parse.py
+++ sub1/parse.py
@@ -45,7 +45,7 @@ def import_data(data_path=DATA_FILE):
     """
     Req. 1-1-1 음식점 데이터 파일을 읽어서 Pandas DataFrame 형태로 저장합니다
     """
-
+    
     try:
         with open(data_path, encoding="utf-8") as f:
             data = json.loads(f.read())
