bf3b8e13e57600e8774d9073e1e9734e293c7614
edenist-x-x <yaron_e5@naver.com>
2021-09-13 06:34:23 +0900

    🔧 , 🔨, 💄 : FE 초기 환경 설정 (nextjs, react, tailwindcss), 메인 페이지 마크업
    Former-commit-id: 72e0b09f9f9c7b39978225c83a0a43fb0aea6eb2

---
+++ sub2/frontend/next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  images: {
+    domains: [
+      "loading.io",
+      "mblogthumb-phinf.pstatic.net",
+      "blog.hmgjournal.com",
+      "c4.wallpaperflare.com",
+      "img1.daumcdn.net",
+      "www.junggi.co.kr",
+    ],
+  },
+};
