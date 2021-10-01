f6db22a817865e1abb65ba9c4bd01ac8534ff558
qudcks3836 <qudcks3836@gmail.com>
2021-10-01 11:22:48 +0900

    modify delete user
    Former-commit-id: 7233e02cfcdcc91ec12ead0acf37a17c3544dcb9

--- ISEAU/backend/user/views.py
+++ ISEAU/backend/user/views.py
@@ -45,12 +45,21 @@ class ProfileUpdateAPI(generics.UpdateAPIView):
 
 
 
+# class Delete(APIView):
+#     permission_classes = (permissions.AllowAny,)
+#     def delete(request, user_pk):
+#         user = get_object_or_404(User, id=user_pk)
+#         print(user)
+#         user.delete()
+#         return Response(user, {'id': user_pk})
+
 class Delete(APIView):
     permission_classes = (permissions.AllowAny,)
-    def delete(request, user_pk):
-        user = get_object_or_404(User, pk=user_pk)
+
+    def delete(self, request, user_pk):
+        user = get_object_or_404(User, id=user_pk)
         user.delete()
-        return Response(user, {'id': user_pk})
+        return Response({'id': user_pk, 'message': '삭제되었습니다'})
 
 
 class EmailUniqueCheck(generics.CreateAPIView):
