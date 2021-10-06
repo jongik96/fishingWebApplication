ddfe9a0c31ba1abc1cba8a88b3c4af855be15f3c
qudcks3836 <qudcks3836@gmail.com>
2021-10-06 14:07:23 +0900

    fix dockerfile
    Former-commit-id: 367e090dad547e11793505872ca0393a7f7ec67c

--- ISEAU/backend/backend/settings.py
+++ ISEAU/backend/backend/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-*16^cyn%(un@^g3=w&w4e-h)0*u&vv01v%mx!_lk-v6aq5#*#7'
 
-SECURE_SSL_REDIRECT = False
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
